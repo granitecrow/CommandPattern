10482f5e97b8e1ed528ba16b1561b9bb4f822370
Jeffrey Cusato <Jeffrey.Cusato@jacobs.com>
2019-12-27 13:06:15 -0500

    Add project files.

---
+++ CommandPattern.Business/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandPattern.Business
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CommandPattern.Business/Repositories/IProductsRepository.cs
@@ -0,0 +1,16 @@
+﻿using CommandPattern.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Business.Repositories
+{
+    public interface IProductRepository
+    {
+        Product FindBy(string articleId);
+        int GetStockFor(string articleId);
+        IEnumerable<Product> All();
+        void DecreaseStockBy(string articleId, int amount);
+        void IncreaseStockBy(string articleId, int amount);
+    }
+}
